ce09f1d943c6dc49eed76e3bf2bb539d9455b127
Julczan <julek111222h@gmail.com>
2025-11-28 15:59:35 +0100

    each player has its own board

---
+++ src/tests/player.test.js
@@ -0,0 +1,13 @@
+import Player from "../components/player";
+import Gameboard from "../components/gameboard";
+import Ship from "../components/ship";
+
+const player = new Player("player");
+const computer = new Player("computer");
+const playerGameboard = new Gameboard();
+const computerGameboard = new Gameboard();
+
+test("computer and real player types", () => {
+  expect(player.board).toEqual(playerGameboard.board);
+  expect(computer.board).toEqual(computerGameboard.board);
+});
